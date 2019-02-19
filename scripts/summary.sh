23d501609e661adf8ff22315324f2daac8abb0ae
Vamshi Basupalli <vamshi_kr@hotmail.com>
2019-02-19 09:04:16 -0600

    enhanced report for README

--- scripts/summary.sh
+++ scripts/summary.sh
@@ -2,24 +2,29 @@
 
 function main {
     local filepath="$1"
+    local count=1
     
     for PKG in $(cat "$filepath"); do
-        local FILEPATH="source_code/$PKG/exception_info.txt"
-        
-        if [[ -f "$FILEPATH" ]]; then
-            local total_exception_count=$(egrep --count --word-regex 'Expection Type::' "$FILEPATH")
-            local reraise_count=$(egrep --count --word-regex 'Reraise::' "$FILEPATH")
-            local base_exception_count=$(egrep --count --word-regex '(Exception|BaseException)' "$FILEPATH")
 
-            #printf "PACKAGE: %-30s TOTAL_EXCEPTION_COUNT: %-5s RERAISE_COUNT: %-5s BASE_EXCEPTION_COUNT: %-5s\n" $PKG $total_exception_count $reraise_count $base_exception_count
+        (
+            cd "source_code/$PKG"
 
-            if [[ $total_exception_count -gt 0 ]]; then
-                printf "| %s | %s | %s (%d%%) | %s (%d%%)\n" $PKG $total_exception_count $reraise_count $(( reraise_count * 100 / total_exception_count )) $base_exception_count $(( base_exception_count * 100 / total_exception_count ))
-            else
-                printf "| %s | %s | %s (%d%%) | %s (%d%%)\n" $PKG $total_exception_count $reraise_count 0 $base_exception_count 0
+            local FILEPATH="exception_info.txt"
+            
+            if [[ -f "$FILEPATH" ]]; then
+                local loc=$(cloc . | egrep '^Python' | awk '{ print $5 }')
+                local total_exception_count=$(egrep --count --word-regex 'Expection Type::' "$FILEPATH")
+                local reraise_count=$(egrep --count --word-regex 'Reraise::' "$FILEPATH")
+                local base_exception_count=$(egrep --count --word-regex '(Exception|BaseException)' "$FILEPATH")
+
+                if [[ $total_exception_count -gt 0 ]]; then
+                    printf "| %d | %s | %s | %s | %s (%d%%) | %s (%d%%)\n" $count $PKG $loc $total_exception_count $reraise_count $(( reraise_count * 100 / total_exception_count )) $base_exception_count $(( base_exception_count * 100 / total_exception_count ))
+                else
+                    printf "| %d | %s | %s | %s | %s (%d%%) | %s (%d%%)\n" $count $PKG $loc $total_exception_count $reraise_count 0 $base_exception_count 0
+                fi
             fi
-
-        fi
+        )
+        count=$(( count + 1 ))
     done
     
 }
